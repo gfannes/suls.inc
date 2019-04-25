963c94a27510f537f48dc48dff0b73d082484b1d
Geert Fannes <geert.fannes@gmail.com>
2019-04-25 23:39:08 +0200

    Fixed message concatenation + cleanup

--- arduino/LoRa_DHT22_TSL2561/LoRa_DHT22_TSL2561.ino
+++ arduino/LoRa_DHT22_TSL2561/LoRa_DHT22_TSL2561.ino
@@ -1,8 +1,3 @@
-
-
-
-
-
 /*******************************************************************************
  * Copyright (c) 2015 Thomas Telkamp and Matthijs Kooijman
  *
@@ -28,7 +23,10 @@
 
 #include <lmic.h>
 #include <hal/hal.h>
-#include <SPI.h>
+#include <DHT.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_TSL2561_U.h>
 
 // LoRaWAN NwkSKey, network session key
 // This is the default Semtech key, which is used by the prototype TTN
@@ -52,7 +50,6 @@ void os_getDevEui (u1_t* buf) { }
 void os_getDevKey (u1_t* buf) { }
 
 String payload;
-static uint8_t mydata[] = "Hello, world!";
 static osjob_t sendjob;
 
 // Schedule TX every this many seconds (might become longer due to duty
@@ -68,66 +65,44 @@ const lmic_pinmap lmic_pins = {
 };
 
 void onEvent (ev_t ev) {
-    Serial.print(os_getTime());
-    Serial.print(": ");
     switch(ev) {
         case EV_SCAN_TIMEOUT:
-            Serial.println(F("EV_SCAN_TIMEOUT"));
             break;
         case EV_BEACON_FOUND:
-            Serial.println(F("EV_BEACON_FOUND"));
             break;
         case EV_BEACON_MISSED:
-            Serial.println(F("EV_BEACON_MISSED"));
             break;
         case EV_BEACON_TRACKED:
-            Serial.println(F("EV_BEACON_TRACKED"));
             break;
         case EV_JOINING:
-            Serial.println(F("EV_JOINING"));
             break;
         case EV_JOINED:
-            Serial.println(F("EV_JOINED"));
             break;
         case EV_RFU1:
-            Serial.println(F("EV_RFU1"));
             break;
         case EV_JOIN_FAILED:
-            Serial.println(F("EV_JOIN_FAILED"));
             break;
         case EV_REJOIN_FAILED:
-            Serial.println(F("EV_REJOIN_FAILED"));
-            break;
             break;
         case EV_TXCOMPLETE:
-            Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
             if(LMIC.dataLen) {
                 // data received in rx slot after tx
-                Serial.print(F("Data Received: "));
-                Serial.write(LMIC.frame+LMIC.dataBeg, LMIC.dataLen);
-                Serial.println();
             }
             // Schedule next transmission
             os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
             break;
         case EV_LOST_TSYNC:
-            Serial.println(F("EV_LOST_TSYNC"));
             break;
         case EV_RESET:
-            Serial.println(F("EV_RESET"));
             break;
         case EV_RXCOMPLETE:
             // data received in ping slot
-            Serial.println(F("EV_RXCOMPLETE"));
             break;
         case EV_LINK_DEAD:
-            Serial.println(F("EV_LINK_DEAD"));
             break;
         case EV_LINK_ALIVE:
-            Serial.println(F("EV_LINK_ALIVE"));
             break;
          default:
-            Serial.println(F("Unknown event"));
             break;
     }
 }
@@ -148,11 +123,6 @@ void setup_sensors(SensorData &calibration);
 void get_sensor_data(SensorData &sensor_data, const SensorData &calibration);
 
 SensorData calibration;
-
-#include <DHT.h>
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_TSL2561_U.h>
 
 #define DHTPIN 5
 #define DHTTYPE DHT22
@@ -202,7 +172,7 @@ void get_sensor_data(SensorData &sensor_data, const SensorData &calibration)
 void do_send(osjob_t* j){
     // Check if there is not a current TX/RX job running
     if (LMIC.opmode & OP_TXRXPEND) {
-        Serial.println(F("OP_TXRXPEND, not sending"));
+        //OP_TXRXPEND, not sending
     } else {
         SensorData sensor_data;
         //Collect all data to send
@@ -211,22 +181,15 @@ void do_send(osjob_t* j){
         //Serialize data into message
         payload = String("t:") + String(sensor_data.temperature) + String(" h:") + String(sensor_data.humidity);
         if (sensor_data.lux_ok)
-            payload = String(" l:") + String(sensor_data.lux);
+            payload += String(" l:") + String(sensor_data.lux);
         else
-            payload = String(" l:x");
+            payload += String(" l:x");
 
         // Prepare upstream data transmission at the next possible time.
-        Serial.print("payload.c_str(): "); Serial.println(payload.c_str());
-        Serial.print("payload.length(): "); Serial.println(payload.length());
         LMIC_setTxData2(1, payload.c_str(), payload.length(), 0);
-        /* LMIC_setTxData2(1, mydata, sizeof(mydata)-1, 0); */
-        Serial.println(F("Packet queued"));
     }
     // Next TX is scheduled after TX_COMPLETE event.
 }
-
-
-
 
 void setup() {
     Serial.begin(115200);
